3161dd1c98df75b1622c478ee8e71209b3d93a74
ira <vikhrova.irina@gmail.com>
2022-11-01 18:12:04 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+
+//Styles 
+
+function buildStyles() {
+    return gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  };
+  
+  exports.buildStyles = buildStyles;
+ 
+  exports.watch = function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+  };
+ 
+
+// function defaultTask(cb) {
+    
+//     cb();
+//   }
+  
+//   exports.default = defaultTask
+
+
+// export default gulp.series(
+//     styles, server, watch
+//   );
