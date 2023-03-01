6a83b76324ffdb57eea67c3bcd9d1cbdff3918b4
ira <vikhrova.irina@gmail.com>
2023-03-01 16:39:50 +0800

    refactoring: toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,18 +1,46 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const navMenu = document.querySelector('.menu');
-    const navToggle = document.querySelector('.page-header__toggle');
-    
+window.addEventListener('DOMContentLoaded', () => {
+
+    const selectors = {
+        menu: '.menu',
+        toggleButton: '.page-header__toggle'
+
+    }
+
+    const states = {
+        toggle: {
+            closed: 'header-toggle--closed',
+            open: 'header-toggle--open'
+        },
+        menu: {
+            closed: 'menu--closed'
+        }
+    }
+
+    const navMenu = document.querySelector(selectors.menu);
+    const navToggle = document.querySelector(selectors.toggleButton);
+
+
 
     navToggle.addEventListener('click', () => {
-        if (navToggle.classList.contains('header-toggle--closed')) {
-            navToggle.classList.remove('header-toggle--closed');
-            navToggle.classList.add('header-toggle--open');
-            navMenu.classList.remove('menu--closed');
-        
-        } else {
-            navToggle.classList.add('header-toggle--closed');
-            navToggle.classList.remove('header-toggle--open');
-            navMenu.classList.add('menu--closed');
-}
-});
+
+        const isClosed = navToggle.classList.contains(states.toggle.closed)
+
+        // if (isClosed) {
+        //     navToggle.classList.remove(states.toggle.closed);
+        //     navToggle.classList.add(states.toggle.open);
+        //     navMenu.classList.remove(states.menu.closed);
+
+        //     return
+        // }
+        // navToggle.classList.add(states.toggle.closed);
+        // navToggle.classList.remove(states.toggle.open);
+        // navMenu.classList.add(states.menu.closed);
+
+
+        navToggle.classList.toggle(states.toggle.open, isClosed);
+        navToggle.classList.toggle(states.toggle.closed, !isClosed);
+        navMenu.classList.toggle(states.menu.closed, !isClosed);
+
+
+    });
 });
